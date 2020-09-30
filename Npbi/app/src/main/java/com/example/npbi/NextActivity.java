a2bc28021031e8469038e564f28d39b487655c9b
VUTHANHUET <71000712+VUTHANHUET@users.noreply.github.com>
2020-09-30 17:09:35 +0700

    1

---
+++ Npbi/app/src/main/java/com/example/npbi/NextActivity.java
@@ -0,0 +1,25 @@
+package com.example.npbi;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NextActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NextActivity.this, PreActivity.class));
+            }
+        });
+    }
+}
